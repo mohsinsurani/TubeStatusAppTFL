d0f8b421884fc6cdc6646931fa15e25dcd8afaa9
Mohsin <mohsin.surani@aitis.co>
2023-11-08 10:50:57 +0000

    Initial Commit

---
+++ TubeStatusApp/TubeStatusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TubeStatusAppApp.swift
+//  TubeStatusApp
+//
+//  Created by Admin on 08/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct TubeStatusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
