03b151fa3dbaf844d3f11bec26f0b04c862ee2c7
Mohsin <mohsinmax786@gmail.com>
2023-11-11 19:38:53 +0000

    appKey removed

--- TubeStatusApp/Configurations/Credentials.swift
+++ TubeStatusApp/Configurations/Credentials.swift
@@ -10,5 +10,5 @@ import Foundation
 // Enum to store app ID and app key
 enum TFLCredentials {
     static let appID = "TubeStatusApp"
-    static let appKey = "4c97de17233640c2b7eba1526ac6e1b3"
+    static let appKey = ""
 }
