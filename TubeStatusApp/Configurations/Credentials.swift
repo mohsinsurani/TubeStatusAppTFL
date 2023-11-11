d92913c3f0687a2b1a386db83df71282dc13eb36
Mohsin <mohsinmax786@gmail.com>
2023-11-11 21:12:13 +0000

    changes in comments

--- TubeStatusApp/Configurations/Credentials.swift
+++ TubeStatusApp/Configurations/Credentials.swift
@@ -2,13 +2,15 @@
 //  config.swift
 //  TubeStatusApp
 //
-//  Created by Admin on 10/11/2023.
+//  Created by Admin on 09/11/2023.
 //
-// This class will handle all cred values in the future for this app
+// This class handles all cred values for this app
 
 import Foundation
 
-// Enum to store app ID and app key which can be retrieved from https://api-portal.tfl.gov.uk/
+// Enum to store app ID and app key
+// app key can be retrieved from https://api-portal.tfl.gov.uk/
+
 enum TFLCredentials {
     static let appID = "TubeStatusApp"
     static let appKey = ""
